37c382d7dafb5c353b6751eeec4bcf585ad4ba71
Mamuti Shukla <mamutishukla11@gmail.com>
2023-05-01 23:05:58 +0530

    my 1st commit

---
+++ Assignment 1.py
@@ -0,0 +1,31 @@
+a= int(input("enter length of a: "))
+b= int (input("enter width of b: "))
+area=(a*b)
+print(area)
+
+'''
+r=int(input("enter radius of circle r: "))
+areaofcircle =(3.14*(r**2))
+print(areaofcircle)
+
+
+a=int(input("enter any number a: "))
+cubeofnumber=(a*a*a)
+print(cubeofnumber)
+
+
+num1=int(input("enter the number,num1: "))
+num2=int(input("enter the number,num2: "))
+num3=num1
+num1=num2
+num2=num3
+print(num1,num2)
+
+
+
+a=int(input("enter basic salary of an employe,a:"))
+da=(10*a)/(100)
+hra=(20*a)/(100)
+ta=(5*a)/(100)
+print(a+da+hra+ta)
+'''
