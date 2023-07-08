8a2552f08cd1bfb2b84ff2b7ea7f4d70d256bf36
Mamutishukla <126497864+Mamutishukla@users.noreply.github.com>
2023-07-08 17:15:44 +0530

    Add files via upload

---
+++ Python New Assignment 2 .py
@@ -0,0 +1,52 @@
+#1.	Write a Python program to convert kilometers to miles?
+
+'''
+print("Enter Kilometer Value: ")
+km = float(input())
+
+miles = km * (0.621371)
+print("\nEquivalent Value in Miles = ", miles)
+'''
+
+
+#2.	Write a Python program to convert Celsius to Fahrenheit?
+'''
+C= float(input('Enter value: '))
+
+F=(C*1.8)+32
+
+print('F value is :',F)
+'''
+
+
+
+#3.	Write a Python program to display calendar?
+'''
+import calendar
+y=int(input("Enter year: "))
+m=int(input("Enter month: "))
+print(calendar.month(y,m))
+
+'''
+
+#4.	Write a Python program to solve quadratic equation?
+
+
+
+
+#5.	Write a Python program to swap two variables without temp variable?
+'''
+x=int(input('enter the value of x: '))
+y=int(input('enter the value of y: '))
+
+
+x=x+y
+y=x-y
+x=x-y
+
+print('new x: ',x)
+print('new y: ',y)
+'''
+
+
+
